49626ba9a681bd6887ea3e5bb0cb512913eff431
dev-willian <91983587+dev-willian@users.noreply.github.com>
2021-11-19 23:08:21 -0300

    salvo-angular-segundo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
+import { CarrinhoComponent } from './pagina/carrinho/carrinho.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsolesComponent } from './pagina/consoles/consoles.component';
 import { ContatoComponent } from './pagina/contato/contato.component';
 import { InicialComponent } from './pagina/inicial/inicial.component';
 import { JogosComponent } from './pagina/jogos/jogos.component';
+import { LoginComponent } from './pagina/login/login.component';
 
 const routes: Routes = [
   { path:'', component: InicialComponent, pathMatch:'full' },
   { path: 'inicial', component: InicialComponent },
   { path: 'contato', component: ContatoComponent },
-  { path: 'Jogos', component: JogosComponent }
+  { path: 'Jogos', component: JogosComponent },
+  { path: 'carrinho', component: CarrinhoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'Consoles', component: ConsolesComponent}
 ];
 
 @NgModule({
